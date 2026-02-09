8081979f176dac9f832e52202b5806d460850036
giggl1ng <linlalex1212@gmail.com>
2026-02-09 16:45:55 -0500

    Delete src directory

--- src/main/java/com/example/airtimebackend/ai/AIProvider.java
+++
@@ -1,7 +0,0 @@
-package com.example.airtimebackend.ai;
-
-import com.example.airtimebackend.model.BrainDumpResponse;
-
-public interface AIProvider {
-    BrainDumpResponse analyzeBrainDump(String text);
-}
--- src/main/java/com/example/airtimebackend/model/BrainDumpRequest.java
+++
@@ -1,5 +0,0 @@
-package com.example.airtimebackend.model;
-
-public class BrainDumpRequest {
-    public String text;
-}
